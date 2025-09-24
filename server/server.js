8b9ff84ec5731b24d695cfe06a98a55326b1e8ba
ayushmishradev <ayushmishrawebdev@gmail.com>
2025-09-24 21:31:11 +0530

    add mongoose

--- server/server.js
+++ server/server.js
@@ -2,10 +2,21 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const Message = require('./models/Message');
 
 const app = express();
 app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/chat-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('✅ Connected to MongoDB');
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: 'http://localhost:3000' }
@@ -14,7 +25,9 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  socket.on('send_message', (data) => {
+  socket.on('send_message', async (data) => {
+    const msg = new Message({ text: data.text });
+    await msg.save()
     io.emit('receive_message', data);
   });
 
